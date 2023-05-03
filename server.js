c8d546ef5179fbf7a657254bfa43b1ae1770d105
sosina yasin <121737266+sosina999@users.noreply.github.com>
2023-05-03 15:44:02 -0400

    client side file created

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const pool = require ('./server/config/database')
 const express = require('express');
-const mysql = require('mysql');
+const userRouter = require('./server/api/users/user.router')
 const cors = require('cors');
 const app = express();
 const PORT= process.env.port;
-
  
-app.use(cors()); //middle ware
-app.use(express.urlencoded({extended: true}));
+app.use(cors()); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.use('/api/users', userRouter)
 
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
 app.listen(PORT,()=>console.log(`listening at http://localhost: ${PORT}`))
